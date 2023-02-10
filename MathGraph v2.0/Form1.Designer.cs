134948f468b686e73c0e3f421cfda0119c32be0a
Lina_ <Lina_@CAINE>
2023-02-10 13:08:53 +0900

    Добавьте файлы проекта.

---
+++ MathGraph v2.0/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace MathGraph_v2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            this.chart1.Size = new System.Drawing.Size(375, 375);
+            this.chart1.SuppressExceptions = true;
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(608, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Square it!!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(393, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 375);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Funcion Type";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 115);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(103, 24);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.Text = "f(x) = kx+b";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 85);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(146, 24);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.Text = "f(x) = ax^2+bx+c";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 55);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(190, 24);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.Text = "f(x) = ax^3+bx^2+cx+d";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(155, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "f(x) = a*sin(b+x)+c";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(664, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(664, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(101, 27);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(664, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(101, 27);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(608, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(608, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(608, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "Func1";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.UseWaitCursor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 56);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 24);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "Func2";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Location = new System.Drawing.Point(608, 177);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(157, 91);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Funcion Selection";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(608, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Square is -->";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(697, 303);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(68, 27);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(608, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "label5";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(664, 134);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(101, 27);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.Text = "0";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Button button1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private GroupBox groupBox2;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+    }
+}
---
+++ MathGraph v2.0/Form1.cs
@@ -0,0 +1,220 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Security.Cryptography.X509Certificates;
+
+
+///
+/// 
+///
+///                    if (radio == 1) { 
+///                        x.Points.AddXY(i / 100, a * Math.Sin(i / 100 * b) + c);
+///                        memory[i] = a * Math.Sin(i / 100 * b);
+///                    }
+///                    if (radio == 2) {
+///                        x.Points.AddXY(i / 100, a * Math.Pow(i / 100, 3) + b * Math.Pow(i / 100, 2) + c * i / 100 + d);
+///                        memory[i] = a * Math.Pow(i / 100, 3) + b * Math.Pow(i / 100, 2) + c * i / 100 + d;
+///                    }
+///                    if (radio == 3) {
+///                        x.Points.AddXY(i / 100, a * i / 100 * i / 100 + b * i / 100 + c);
+///                        memory[i] = a * i / 100 * i / 100 + b * i / 100 + c;
+///                    }
+///                    if (radio == 4) {
+///                        x.Points.AddXY(i / 100, a * i + b);
+///                        memory[i] = a * i / 100 + b;
+///                    }
+/// 
+namespace MathGraph_v2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label5.Hide();
+            textBox1.Hide();
+            textBox2.Hide();    
+            textBox3.Hide();
+            textBox5.Hide();
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+
+        }
+
+        public int radio;
+
+        Series f1 = new Series();
+        Series f2 = new Series();
+        Series square = new Series();
+        double[] memory_f1 = new double[100];
+        double[] memory_f2 = new double[100];
+        public int func; 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            square.Points.Clear();
+            double summ = 0;
+            for (int i = 0; i < memory_f1.Count(); i++) {
+                summ += (memory_f1[i]/ memory_f1.Count() - memory_f2[i]/ memory_f1.Count());
+                square.Points.AddXY(Convert.ToDouble(i) / 100, memory_f1[i]);
+                square.Points.AddXY(Convert.ToDouble(i) / 100, memory_f2[i]);
+            }
+            square.ChartType = SeriesChartType.Line;
+            chart1.Series.Add(square);
+            textBox4.Text = summ.ToString();
+        }
+
+        public void update_function(object sender, EventArgs e, Series x) 
+        {
+            x.ChartType = SeriesChartType.Line;
+            x.Points.Clear();
+            
+                double []memory = new double[100];
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                double c = Convert.ToDouble(textBox3.Text);
+                double d = Convert.ToDouble(textBox5.Text);
+                for (int i = 0; i < 100; i++)
+                {
+                    double p = Convert.ToDouble(i) / 100;
+                    if (radio == 1)
+                    {
+                        x.Points.AddXY(p, a * Math.Sin(p * b) + c);
+                        memory[i] = a * Math.Sin(p * b)+c;
+                    }
+                    if (radio == 2) 
+                    {
+                        x.Points.AddXY(p, a * p * p * p + b * p * p + c * p + d);
+                        memory[i] = a* p *p * p + b * p * p + c * p + d;
+                    }
+                    if (radio ==3) 
+                    {
+                        x.Points.AddXY(p, a * p * p + b * p + c);
+                        memory[i] = a*p*p+ b*p +c;
+                    }
+                    if (radio == 4) 
+                    {
+                        x.Points.AddXY(p, a * p + b);
+                        memory[i] = a * p + b;
+                    }
+
+                }         
+                if (func ==1) memory_f1 = memory;
+                else memory_f2 = memory;
+                chart1.Series.Add(x);
+                for (int i = 0; i < 100;i++)
+                    Console.WriteLine(memory[i]);
+             }
+            catch (Exception ex) { Console.WriteLine("Some Errors was detected!"); }
+            finally {}
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "a";
+            label2.Text = "b";
+            label3.Text = "c";
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            textBox1.Show();
+            textBox2.Show();
+            textBox3.Show();
+            radio = 1;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (func == 1)
+                update_function(sender, e, f1);
+            else update_function(sender, e, f2);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+            if (func == 1)
+                update_function(sender, e, f1);
+            else update_function(sender, e, f2);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+            if (func == 1)
+                update_function(sender, e, f1);
+            else update_function(sender, e, f2);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            func = 1;
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            func = 2;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "a";
+            label2.Text = "b";
+            label3.Text = "c";
+            label5.Text = "d";
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            label5.Show();
+            textBox1.Show();
+            textBox2.Show();
+            textBox3.Show();
+            textBox5.Show();
+            radio = 2;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+            radio = 3; 
+            label1.Text = "a";
+            label2.Text = "b";
+            label3.Text = "c";
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            textBox1.Show();
+            textBox2.Show();
+            textBox3.Show();
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "k";
+            label2.Text = "b";
+            label1.Show();
+            label2.Show();
+            textBox1.Show();
+            textBox2.Show();
+            radio = 4;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (func == 1)
+                update_function(sender, e, f1);
+            else update_function(sender, e, f2);
+        }
+    }
+}
